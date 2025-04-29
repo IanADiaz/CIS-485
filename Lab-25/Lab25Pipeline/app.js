04ae4aa3012eb57aab6ea70d066fa3099d5a0a35
iandiaz <ian.a.diaz@outlook.com>
2025-04-29 17:55:22 -0400

    addition of a new feature

--- Lab-25/Lab25Pipeline/app.js
+++ Lab-25/Lab25Pipeline/app.js
@@ -11,6 +11,11 @@ app.get('/text', (req, res) => {
 	res.send('Testing');
 });
 
+app.get('/feature', (req, res) => {
+        res.send('This is my feature');
+});
+
+
 PORT = 3000;
 
 app.listen(PORT,() => {
